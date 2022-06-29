2c4fbb346c09dff105bcdcd270ccabd647acb18c
Abolior <dimamatveev121@gmail.com>
2022-06-29 23:17:04 +0500

    bly

--- main.py
+++ main.py
@@ -1,41 +1,71 @@
-import random 
-import math
+# import random
+# import math
+#
+#
+# def get_number():
+#     need = round(random.random() * 100)
+#
+#     return need
+#
+#
+# def say_number(num, need):
+#     if need > num:
+#         return 1
+#     elif need < num:
+#         return 0
+#     else:
+#         return 'Ура!'
+#
+#
+# if __name__ == "__main__":
+#     need = get_number()
+#     max = 100
+#     num = max / 2
+#
+#     ost = 50
+#
+#     while True:
+#         ans = say_number(num, need)
+#
+#         ost = math.ceil(ost / 2)
+#
+#         if ans == 1:
+#             num = num + ost
+#         elif ans == 0:
+#             num = num - ost
+#         else:
+#             break
+#         print(num)
+#
+#     print(need)
 
 
-def get_number():
-	need = round(random.random() * 100)
+name = [100, 240]
 
-	return need
-
-def say_number(num, need):
-	if need > num:
-		return 1
-	elif need < num:
-		return 0
-	else:
-		return 'Ура!'
+width = name[0]
+height = name[1]
 
 
+def square(name):
+    if (name[0] > name[1]):
+        max = name[0]
+        min = name[1]
+    else:
+        max = name[1]
+        min = name[0]
 
-if __name__ == "__main__":
-	need = get_number()
-	max = 100
-	num = max/2
+    if (max % min) == 0:
+        return name
 
-	ost = 50
+    else:
+        ever = max // min
+        r = max - ever * min
+        new_square = [min, r]
 
-	while True:
-		ans = say_number(num, need)
-		
-		ost = math.ceil(ost/2)
-		
-		if ans == 1:
-			num = num + ost
-		elif ans == 0:
-			num = num - ost
-		else:
-			break
-		print(num)
-			
+        if max > 20:
+            square(new_square)
+        else:
+            return min
 
-	print(need)
+
+print(square(name))
